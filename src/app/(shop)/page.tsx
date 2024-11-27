abe0216f55be9cc6043f804fbd67bbf27a2ee300
Nelson Cadenas <nca1478@yahoo.com>
2024-11-27 15:51:57 -0400

    192. TopMenu

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,8 +3,7 @@ import { titleFont } from "../config/fonts";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl">Hola Mundo!!!</h1>
-      <h1 className={`${titleFont.className} text-5xl`}>Hola Mundo!!!</h1>
+      <h1 className={`${titleFont.className} text-5xl`}>Home Page</h1>
     </div>
   );
 }
