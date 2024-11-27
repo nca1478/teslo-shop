a36fefbdef0ae6c290030c0a56a154cfca065c01
Nelson Cadenas <nca1478@yahoo.com>
2024-11-27 18:08:51 -0400

    193. Página 404 - Categorías

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,19 @@
-export default function CategoryPage() {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+
+export default async function CategoryPage({ params }: Props) {
+  const { id } = await params;
+
+  if (id === "kids") {
+    notFound();
+  }
+
   return (
     <div>
-      <h1 className="text-5xl">Category Page</h1>
+      <h1 className="text-5xl">Category Page {id}</h1>
     </div>
   );
 }
