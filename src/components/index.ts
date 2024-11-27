36b6b909f1b94f01c52d7f11f56e09aaa3077428
Nelson Cadenas <nca1478@yahoo.com>
2024-11-27 18:52:33 -0400

    196. Grid de Productos y Tarjeta del Producto

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+// Products
+export { ProductGrid } from "./products/product-grid/ProductGrid";
+export { ProductGridItem } from "./products/product-grid/ProductGridItem";
+
+// Ui
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { Title } from "./ui/title/Title";
 export { TopMenu } from "./ui/top-menu/TopMenu";
