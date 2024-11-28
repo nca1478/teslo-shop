90c1bfa0f2fe5a8617c68c9a93e11cde8b87b844
Nelson Cadenas <nca1478@yahoo.com>
2024-11-28 15:04:09 -0400

    213. Slideshow con Thumbs - Parte 2

--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 import { Swiper as SwiperObject } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Autoplay, FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -34,8 +34,13 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
         }
         spaceBetween={10}
         navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
+        autoplay={{
+          delay: 2500,
+        }}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
+        modules={[FreeMode, Navigation, Thumbs, Autoplay]}
         className="mySwiper2"
       >
         {images.map((image) => (
@@ -43,6 +48,28 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
             <Image
               width={1024}
               height={800}
+              src={`/products/${image}`}
+              alt={title}
+              className="rounded-lg object-fill"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <Image
+              width={300}
+              height={300}
               src={`/products/${image}`}
               alt={title}
               className="rounded-lg object-fill"
