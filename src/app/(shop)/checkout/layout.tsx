9239799d8c83819a574c89f0f4cfd7ff5bd146ba
Nelson Cadenas <nca1478@yahoo.com>
2024-12-05 21:51:15 -0400

    312. Middleware

---
+++ src/app/(shop)/checkout/layout.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/config/auth/auth.config";
+import { redirect } from "next/navigation";
+
+export default async function CheckoutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  if (!session?.user) {
+    // redirect('/auth/login?returnTo=/perfil');
+    // redirect("/auth/login?redirectTo=/checkout/address");
+    redirect("/auth/login");
+  }
+
+  return <>{children}</>;
+}
